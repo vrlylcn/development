1275c7744b08675adee036f3365d4e7452a3dcad
vuralyalcinn <vural.yalcin@akinon.com>
2021-03-25 22:57:33 +0300

    Document title changed

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Vural Yalçın - Frontend Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
